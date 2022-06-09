2ed57fb6970bc9a77e008501b79838d363a4cbc9
AldaLatifah_010 <aldaa.ltfh@gmail.com>
2022-06-09 15:28:44 +0800

    perubahan layout pake constraint, camera activity done

---
+++ app/src/main/java/com/example/temantani/view/detection/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.example.temantani.view.detection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.temantani.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
--- app/src/main/java/com/example/temantani/view/main/MainActivity.kt
+++ app/src/main/java/com/example/temantani/view/main/MainActivity.kt
@@ -1,27 +1,46 @@
 package com.example.temantani.view.main
 
 import android.Manifest
-import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.View
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.temantani.data.local.Data
 import com.example.temantani.data.model.MenuCategory
 import com.example.temantani.databinding.ActivityMainBinding
-import com.example.temantani.databinding.MenuViewBinding
+import com.example.temantani.databinding.CardMenuBinding
 import com.example.temantani.view.detection.CameraActivity
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var mapMenuCategory: Map<String, MenuCategory>
-
     private val viewModel by viewModels<MainViewModel>()
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Izin tidak diberikan.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,15 +50,20 @@ class MainActivity : AppCompatActivity() {
 
         mapMenuCategory = viewModel.getMenuCategory()
 
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+
         supportActionBar?.hide()
 
         setupLayout()
-
-
+        setupAction()
 
     }
-
-
 
     private fun setupLayout() {
         binding.apply {
@@ -51,17 +75,27 @@ class MainActivity : AppCompatActivity() {
             setupModuleCard(Data.MENU_COMMUNITY, miOption5)
         }
     }
-    private fun setupModuleCard(mapKey: String, moduleCard: MenuViewBinding) {
+    private fun setupModuleCard(mapKey: String, moduleCard: CardMenuBinding) {
         val category = mapMenuCategory[mapKey]
         if (category != null) {
             Glide.with(this@MainActivity)
                 .load(category.thumbnail)
                 .apply(RequestOptions().override(100, 100))
-                .into(moduleCard.miOption0.ivItemImage)
-            moduleCard.title.text = category.title
+                .into(moduleCard.ivItemImage)
         }
     }
 
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun setupAction(){
+        binding.apply {
+            miOption0.miContainer.setOnClickListener{
+                startCameraX()
+            }
+        }
+    }
 
     private fun startCameraX() {
         val intent = Intent(this, CameraActivity::class.java)
