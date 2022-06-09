a4fe0cdc9f66f43795dbeaa9cbbf054e6b074a44
AldaLatifah_010 <aldaa.ltfh@gmail.com>
2022-06-09 22:50:47 +0800

    Activity Treatment, Calculator and Fragmet Diseases, Plant

---
+++ app/src/main/java/com/example/temantani/view/treatment/TreatmentActivity.kt
@@ -0,0 +1,12 @@
+package com.example.temantani.view.treatment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.temantani.R
+
+class TreatmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_treatment)
+    }
+}
